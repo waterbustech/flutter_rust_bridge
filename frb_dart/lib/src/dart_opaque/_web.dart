f58509660e47525398647848b4b462387deae086
lambiengcode <lambiengcode@gmail.com>
2024-08-20 14:40:59 +0700

    remove dart:js_util

--- frb_dart/lib/src/dart_opaque/_web.dart
+++ frb_dart/lib/src/dart_opaque/_web.dart
@@ -1,5 +1,3 @@
-import 'dart:js_util';
-
 import 'package:flutter_rust_bridge/src/dart_opaque/_common.dart';
 import 'package:flutter_rust_bridge/src/generalized_frb_rust_binding/generalized_frb_rust_binding.dart';
 import 'package:flutter_rust_bridge/src/platform_types/platform_types.dart';
@@ -12,8 +10,8 @@ PlatformPointer encodeDartOpaque(Object raw, NativePortType dartHandlerPort,
 
 Object _prepareDartOpaque(Object raw) {
   // #2183
-  if (raw is Function) {
-    return allowInterop(raw);
-  }
+  // if (raw is Function) {
+  //   return raw;
+  // }
   return raw;
 }
